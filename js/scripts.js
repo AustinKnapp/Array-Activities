1269380b3190dbf64d557659a819e7469ccf142c
Austin Knapp and Joel Waage <austin+joel@>
2016-06-20 12:45:04 -0700

    add second array in proper order

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 var favorite = [];
+var favorite2 = [];
 var food = '';
 var animal = '';
 var city = '';
 var water = '';
 var movie = '';
+var coffee = '';
 
 
 $(document).ready(function() {
@@ -13,10 +15,14 @@ $(document).ready(function() {
   city = $("input#city").val();
   water = $("input#water").val();
   movie = $("input#movie").val();
+  coffee = $("input#coffee").val();
 
-  favorite.push(food, animal, city, water, movie);
+  favorite.push(food, animal, city, water, movie, coffee);
+  favorite2.push(favorite[1], favorite[0], favorite[2]);
+
 
   console.log(favorite);
+  console.log(favorite2);
 
   event.preventDefault();
   });
